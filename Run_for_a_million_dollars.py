ab23cd79aa2c4afd18a3d3daaeba021e73c310e3
jonathan-hoh <48967547+jonathan-hoh@users.noreply.github.com>
2019-11-16 19:40:00 -0700

    Run this and win a million dollars
    I aint frontin, you will instantly win a million dollars if you are able to beat this game. Requires emoji package for full effect.

---
+++ Run_for_a_million_dollars.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 11:22:45 2019
+
+@author: hohjo
+"""
+import time
+import emoji
+
+print ("hey there friend, whats your name?")       
+name = input()
+time.sleep(2)
+print (emoji.emojize(name+" is fucking stupid :broken_heart:"))
